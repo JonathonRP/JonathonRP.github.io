e9504dfd72b7e30759609e56fd7fec6001cf5615
JonathonRP <jreesep@gmail.com>
2024-10-13 23:13:14 +0000

    update ci

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
 	// your configuration options here...
 	// https://docs.astro.build/en/reference/configuration-reference/
+	site: 'https://JonathonRP.github.io',
 	integrations: [svelte()],
 	vite: {
 		server: {
